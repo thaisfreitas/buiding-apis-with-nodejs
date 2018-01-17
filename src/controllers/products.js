57bece8b928c939da042bbb05c691f06b099741f
Thais Moura <tfreitas@thoughtworks.com>
2018-01-16 22:40:50 -0300

    feat: add the get product controler method

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,5 +1,13 @@
 class ProductsController {
 
+    get(req, res) {
+        return res.send([{
+            name: 'Default Product',
+            description: 'product description',
+            price: 100
+        }])
+    }
+
 }
 
 export default ProductsController;
